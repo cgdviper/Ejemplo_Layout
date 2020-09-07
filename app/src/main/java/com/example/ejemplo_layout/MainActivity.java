439e6ead4ed5ba13b6a3c29834481d50a9accfe4
ThinkPad T490 <cgd.santo.tomas@gmail.com>
2020-09-07 15:46:02 -0300

    v1

---
+++ app/src/main/java/com/example/ejemplo_layout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ejemplo_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejemplo_grid_layout);
+    }
+}
